6d41093f58a9ba1a816621c030afacedf1b69a33
MUHAMAD ABDUL HADI <60739072+hadiok@users.noreply.github.com>
2024-07-28 14:43:07 +0700

    u

---
+++ head.php
@@ -0,0 +1,5 @@
+<!-- head.php -->
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="Deskripsi situs web Anda">
+<meta name="keywords" content="keyword1, keyword2">
